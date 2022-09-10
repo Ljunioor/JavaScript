3fd431516860914dd40141bc85d4f13e718816cc
Alves <leonardojunior217@gmail.com>
2022-09-10 00:17:53 -0300

    not

---
+++ origamid/Funcoes/script.js
@@ -0,0 +1,15 @@
+var numeral=5*5;
+console.log(numeral)
+
+function medirArea (lado) {
+    return lado * lado;
+}
+medirArea(20)
+console.log(medirArea(20));
+
+function pi() {
+    return 3.14;
+}
+
+var total = 5 * pi();
+on
