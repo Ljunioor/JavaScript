72fcfb415264cb013b6e93b350498020e2f05259
Ljunioor <104178015+Ljunioor@users.noreply.github.com>
2022-09-16 22:57:31 -0300

    add

--- Projetos/Aparecer Behance/leonardobandeira.js
+++ Projetos/Aparecer Behance/leonardobandeira.js
@@ -1,9 +1,26 @@
-const date = new Date()
-console.log(date)
+const date = new Date();
+console.log(new Date());
 
 
-if(date < 09/10/2020){
+
+var time = date.getTime();
+console.log(time + ' time');
+
+
+
+const leonardo = document.getElementById('leonardo').classList;
+
+if(date >= 0000){
     console.log('DATA MAIOR')
+    //aqui ele remove
+    leonardo.remove('desactive')
 } else {
+    //aqui ele add none
+    leonardo.add('desactive')
     console.log('DATA MENOR')
 }
+
+//const leonardo = document.getElementsByClassName('leonardo');
+    
+    
+    
