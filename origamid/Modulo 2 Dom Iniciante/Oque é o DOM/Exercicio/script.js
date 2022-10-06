9fc83fc397e03f0802e2a79e09460c43893762c9
Ljunioor <104178015+Ljunioor@users.noreply.github.com>
2022-10-06 20:51:34 -0300

    DOM

---
+++ origamid/Modulo 2 Dom Iniciante/Oque é o DOM/Exercicio/script.js
@@ -0,0 +1,14 @@
+//Retorne o url da página atual utilizando o objeto window
+const hrefPagina = window.location.href;
+console.log(hrefPagina);
+
+// Seleciona o primeiro elemento da página que possua classe ativo
+const elementoAtivo = document.querySelector('.ativo')
+console.log(elementoAtivo)
+//Retorne a linguagem do navegador
+const navegador = window.navigator.language;
+console.log(navegador)
+//Retorne a largura da janela
+
+const largura = window.innerWidth;
+console.log(largura);
