4a9568380a8d09e8321e8b382d4fc11b844a2c87
Ljunioor <104178015+Ljunioor@users.noreply.github.com>
2023-03-20 13:25:56 -0300

    exercicios

--- origamid/Modulo 3 Objetos/4 - String/Exercicios/script-Exercicio.js
+++ origamid/Modulo 3 Objetos/4 - String/Exercicios/script-Exercicio.js
@@ -1,48 +1,76 @@
-//transforme o objeto abaixo em uma ocnstruction function
-/*const pessoa = {
-    nome: 'nome Pessoa',
-    idade: 0,
-    andar(){
-        console.log(this.nome + 'andou');
+//Utilizando foreach no array abaixo, some os valores de taxa e os valores de recebimento
+
+const transacoes = [ 
+    {
+        descrição:'Taxa do Pão',
+        valor: 'R$39',
+    },
+    {
+        descrição:'Taxa do Mercado',
+        valor:'R$129',
+    },
+    {
+        descrição:'Recebimento de Cliente',
+        valor: 'R$99',
+    },
+    {
+        descrição:'Taxa do Banco',
+        valor: 'R$129',
+    },
+    {
+        descrição:'Recebimento de Cliente',
+        valor: 'R$49',
+    },
+];
+
+let taxaTotal = 0;
+let recebimentoTotal = 0;
+transacoes.forEach((element)=>{
+    const numLimpo = +element.valor.replace('R$','');
+    
+    if (element.descrição.slice(0,4) === 'Taxa'){
+    taxaTotal = taxaTotal + numLimpo}
+    else{
+        recebimentoTotal = recebimentoTotal + numLimpo
     }
-}*/
-function pessoa(nome, idade) {
-this.nomePessoa = nome,
-this.idadePessoa = idade +' anos',
-this.andar = function(xMetros){
- console.log(this.nomePessoa + ' andou ' + xMetros + ' metros.');
-}
+});
 
-};
+console.log(taxaTotal);
+console.log(recebimentoTotal);
 
 
-const João = new pessoa('João', 20);
+//Retone uma array com a lista abaixo
+const transportes = 'Carro;Avião;Trem;ônibus;Bicileta';
 
-const Maria = new pessoa('Maria', 25);
+const arrayTranspotes = transportes.split(';');
+console.log(arrayTranspotes);
 
-const Bruno = new pessoa('Bruno', 25);
+//Substitua todos os spans por a
 
-//console.log(Maria.andar(20))
+let html = `<ul>
+                <li><span>Sobre</span></li>
+                <li><span>Produtos</span></li>
+                <li><span>Contato</span></li>
+              <ul>`;
 
-function Dom(elementoDom) {
-    const elemento = document.querySelectorAll(elementoDom);
-    this.elementos = elemento,
-    this.adiconaClass = function(classeAdd){
-        this.elementos.forEach((element) =>{
-            element.classList.add(classeAdd);
-        })
-    }
-    this.removeClas = function(classeR){
-        this.elementos.forEach((element) =>{
-            element.classList.remove(classeR);
-        })
-        
-    }
-};
+html = html.split('span').join('a');
 
-const selecionaLI = new Dom('li');
-selecionaLI.adiconaClass('nomeClass');
+console.log(html)
 
-selecionaLI.removeClas('ativo')
+//retorne o ultimo caractere da frase
+const frase = ' Melhor do ano!';
+console.log(frase.slice(-1));
+//retorne o total de taxas
 
+const transacoes2 = ['Taxa do Banco', '  TAXA DO PÃO', ' taxa do mercado', 'depósito Bancário', 'Tarifa especial'];
 
+let taxasTotal2 = 0;
+
+ transacoes2.forEach((item)=>{
+    console.log(item.toLowerCase().trim().slice(0,4))
+    if(item ==='taxa');
+        taxasTotal2 = taxasTotal2 + 1
+    })
+    
+ 
+ console.log(taxasTotal2);
