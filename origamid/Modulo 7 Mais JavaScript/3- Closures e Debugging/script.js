de7e777167f6c4ca9082bce705fee81f16bdce14
Ljunioor <104178015+Ljunioor@users.noreply.github.com>
2023-07-28 13:44:02 -0300

    Closure

---
+++ origamid/Modulo 7 Mais JavaScript/3- Closures e Debugging/script.js
@@ -0,0 +1,34 @@
+//Escopo
+/*let item1 = 1;
+
+function funcao1(){
+    let item2= 2;
+    debugger;
+    function funcao2(){
+        let item3= 3;
+        console.log(item1);
+        console.log(item2);
+        console.log(item3);
+    }
+    funcao2();
+}
+funcao1()*/
+
+//Comum
+//debugger;
+/*function contagem(){
+    let total = 0;
+    return function incremento(){
+        total++;
+        console.log(total);
+    }
+}
+const ativarIncremento = contagem();
+ativarIncremento();
+ativarIncremento();
+ativarIncremento();
+ativarIncremento();
+ativarIncremento();
+*/
+
+
